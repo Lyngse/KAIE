e4f98d8fb26e2313f0b64f40f60eafa601f59489
Lyngse <superlyng@msn.com>
2016-03-27 15:06:32 +0200

    Hello?

---
+++ KAIE/KAIE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KAIE.Startup))]
+namespace KAIE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
